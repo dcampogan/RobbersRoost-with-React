be722b66ae392b2bdfb3d31640dc10a7eaaf2a00
Dwayne Campogan <dcampogan@gmail.com>
2020-01-15 14:09:26 -0800

    19_01_15 - Added cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Trips from './components/TripsComponent'
+import { TRIPSDATA } from './shared/tripsData';
 
-function App() {
-  return (
-      <React.Fragment>
-        HELLO WORLD FROM APP
-      </React.Fragment>
-  );
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          trips: TRIPSDATA
+      };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar className="navcolordc">
+          <div className="container">
+            <NavbarBrand href="/">Robbers Roost</NavbarBrand>
+          </div>
+        </Navbar>
+        <div className="container">
+          <div className="row">
+            <Trips trips={this.state.trips}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/TripsComponent.js
+++ src/components/TripsComponent.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
 class Trips extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      trips
+      selectedTrip: null
     };
   }
+  onTripSelect(trip) {
+    this.setState({ selectedTrip: trip });
+  }
+
+  renderSelectedTrip(trip) {
+    if (trip) {
+      return (
+        <Card>
+          <CardImg top src={trip.image} alt={trip.name} />
+          <CardBody>
+            <CardTitle>{trip.name}</CardTitle>
+            <CardText>{trip.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    }
+    return <div />;
+  }
+
   render() {
+    const trips = this.props.trips.map(trip => {
+      return (
+        <div key={trip.id} className="col-md-5 mx-auto my-3">
+          <Card onClick={() => this.onTripSelect(trip)}>
+            <CardImg top max-width="50%" src={trip.image} alt={trip.name} />
+            <CardBody>
+              <CardTitle>{trip.name}</CardTitle>
+              <CardSubtitle>Description</CardSubtitle>
+              <CardText>
+                {trip.description}
+              </CardText>
+              <Button>Button</Button>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    });
+
     return (
-      <div />
-    )
+      <div className="container">
+        <div className="row">{trips}</div>
+        <div className="row">
+          <div className="col-md-5 m-auto">
+            {this.renderSelectedTrip(this.state.selectedTrip)}
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
