f043d68e240778a2b99cfa3b8c8b2cce5d2acef6
Dwayne Campogan <dcampogan@gmail.com>
2020-01-14 17:44:19 -0800

    Initial Commit - React Project Template

---
+++ src/components/TripsComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Trips extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      trips
+    };
+  }
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+
+export default Trips;
